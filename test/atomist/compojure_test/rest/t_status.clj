f36de4930f6ea1b10485a998c045bd38ee9089e5
Ben Griffiths <mrbengriffiths@googlemail.com>
2016-10-27 14:30:44 +0100

    Initial commit from the atomist bot

---
+++ test/atomist/compojure_test/rest/t_status.clj
@@ -0,0 +1,11 @@
+(ns atomist.compojure-test.rest.t-status
+  (:require [atomist.compojure-test.rest.status :as status]
+            [midje.sweet :refer :all]))
+
+(fact-group
+ :unit
+
+ (fact "status returns OK with the correct version"
+       (status/get-status {:version "1.0.0"})
+       =>
+   (contains {:status 200 :body (contains {:status "OK" :version "1.0.0"})})))
